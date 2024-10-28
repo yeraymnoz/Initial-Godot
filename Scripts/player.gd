a403b1d429a9fdaa8dc71de68662f3848a67555a
Yeray <yeraymnoz@gmail.com>
2024-10-28 12:38:21 +0100

    Fix_walk

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,30 +6,45 @@ const JUMP_VELOCITY = -250.0
 
 @onready var anim_tree = $AnimationTree
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 enum {IDLE,WALK,ATTACK,DEAD}
-var baseAnim = IDLE
+var baseAnim
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		isJumping()
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	handle_animations()
+	
+	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
+	
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
-		handle_animations()
+		# Flip the Sprite
+		if direction > 0:
+			animated_sprite.flip_h = false
+		elif direction < 0:
+			animated_sprite.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	# Call animations
+	if is_on_floor():
+		if direction == 0:
+			baseAnim = IDLE
+		else:
+			baseAnim = WALK
+		# Handle jump
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		# Add the gravity
+		velocity.y += gravity * delta
+		isJumping()
 
 	move_and_slide()
 	
